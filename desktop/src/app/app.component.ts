2421087610358865ed2bcd00401b7c108146e4ac
Il Ung <ilungj@gmail.com>
2017-09-01 01:46:57 -1000

    Add method to restrict window resizing to just the app content section without using jQuery.

--- desktop/src/app/app.component.ts
+++ desktop/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  ngOnInit() {
+    let app = this.el.nativeElement.querySelector('.app');
+    this.renderer.setStyle(app, 'height', (window.innerHeight - 30) + 'px');
+
+    window.onresize = () => {
+      this.renderer.setStyle(app, 'height', (window.innerHeight - 30) + 'px');
+    };
+  }
 }
