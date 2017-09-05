e57aa2d4514cdfb1589b6a5a9a9569afa69fa01c
Il Ung <ilungj@gmail.com>
2017-09-04 19:53:09 -1000

    Initialize auth service that will be used for authentication.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 
+import { AuthService } from './auth.service';
+
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
@@ -24,7 +26,7 @@ const routes: Routes = [
   exports: [
     RouterModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
