2d8855a148b49f0de4a5d12a4513e236771f816b
Il Ung <ilungj@gmail.com>
2017-09-04 19:50:17 -1000

    Add sign-in component with animations and routes.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'sign-in', pathMatch: 'full'},
+  {path: 'sign-in', component: SignInComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ web/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css'],
+  animations: [
+    trigger('signInAnim', [
+      transition('void => *', [
+        style({transform: 'translateY(40px)'}),
+        animate('1s ease', style({transform: 'translateY(0)'}))
+      ]),
+
+      transition(':leave',
+        animate('1s ease', style({transform: 'translateX(-40px)', opacity: 0}))
+      )
+    ])
+  ]
+})
+export class SignInComponent implements OnInit {
+
+  // Trick for triggering the leave animation.
+  anim: boolean;
+
+  constructor() {
+    this.anim = true;
+  }
+
+  ngOnInit() {
+  }
+
+}
