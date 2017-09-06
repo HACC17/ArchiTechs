db37127dfeea72593b3c1e57083a531080473217
Il Ung <ilungj@gmail.com>
2017-09-06 09:55:47 -1000

    Move calendar making functionality from calendar component to scheduler service.

--- web/src/app/calendar/calendar.component.ts
+++ web/src/app/calendar/calendar.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { animate, trigger, transition, style } from '@angular/animations';
-import * as moment from 'moment';
-
-export interface Training {
-  month: number,
-  date: number,
-  time: string,
-}
+import {SchedulerService} from '../scheduler/scheduler.service';
 
 @Component({
   selector: 'app-calendar',
@@ -24,36 +18,12 @@ export interface Training {
 export class CalendarComponent implements OnInit {
 
   calendar = [];
-  trainingsOfMonth: any;
 
-  constructor() {
-    this.trainingsOfMonth = [
-      {month: 9, date: 1, time: '10:00 AM - 2:00 PM'},
-      {month: 9, date: 2, time: '9:00 AM - 3:00 PM'}
-    ];
+  constructor(private scheduler: SchedulerService) {
+
   }
 
   ngOnInit() {
-    // Inspired by https://stackoverflow.com/a/39803848.
-    const startWeek = moment().startOf('month').week();
-    const endWeek = moment().endOf('month').week();
-
-    for (let week = startWeek; week <= endWeek; week++) {
-      this.calendar.push({
-        week: week,
-        days: Array(7).fill(0).map((n, i) => {
-          const day = moment().week(week).startOf('week').clone().add(n + i, 'day');
-          const trainingsOfDay = [];
-
-          for (const session of this.trainingsOfMonth) {
-            if (session.date === day.date()) {
-              trainingsOfDay.push(session);
-            }
-          }
-          return {day: day, trainingsOfDay: trainingsOfDay};
-        })
-      })
-    }
-
+    this.calendar = this.scheduler.makeCalendar();
   }
 }
--- web/src/app/scheduler/scheduler.service.ts
+++ web/src/app/scheduler/scheduler.service.ts
@@ -1,13 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import * as moment from 'moment';
 
 @Injectable()
 export class SchedulerService {
 
+
   user: Object;
+  trainingsOfMonth: any;
 
   constructor(private http: Http) {
     this.user = {};
+    this.trainingsOfMonth = [
+      {month: 9, date: 1, time: '10:00 AM - 2:00 PM'},
+      {month: 9, date: 2, time: '9:00 AM - 3:00 PM'}
+    ];
+  }
+
+  makeCalendar(): Object[] {
+    // Inspired by https://stackoverflow.com/a/39803848.
+    const calendar = [];
+    const startWeek = moment().startOf('month').week();
+    const endWeek = moment().endOf('month').week();
+
+    for (let week = startWeek; week <= endWeek; week++) {
+      calendar.push({
+        week: week,
+        days: Array(7).fill(0).map((n, i) => {
+          const day = moment().week(week).startOf('week').clone().add(n + i, 'day');
+          const trainingsOfDay = [];
+
+          for (const session of this.trainingsOfMonth) {
+            if (session.date === day.date()) {
+              trainingsOfDay.push(session);
+            }
+          }
+          return {day: day, trainingsOfDay: trainingsOfDay};
+        })
+      })
+    }
+
+    return calendar;
   }
 
   getData(): Promise<Object> {
