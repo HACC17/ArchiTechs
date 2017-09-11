ff35b4f7806ca02c692f2fc37a59b60ff7034523
Il Ung <ilungj@gmail.com>
2017-09-11 00:19:40 -1000

    Change how current training schedule is displayed on the side bar.

--- web/src/app/user.service.ts
+++ web/src/app/user.service.ts
@@ -5,11 +5,13 @@ import { Http, Response } from '@angular/http';
 export class UserService {
 
   user: any;
+  dateString: string;
 
   constructor(private http: Http) {
     this.user = {};
     // So that we don't get undefined error before api call fills it in.
     this.user.training = {};
+    this.dateString = '';
   }
 
   getUser(): Promise<Object> {
@@ -18,6 +20,8 @@ export class UserService {
       .toPromise()
       .then((res: Response) => {
         this.user = res.json();
+        // The date property is stored to json as string, so convert it back to date object.
+        this.dateString = new Date(this.user.training.date).toDateString();
         return this.user;
       });
   }
@@ -25,6 +29,7 @@ export class UserService {
   // Takes the new training schedule and updates it on the database with the user.
   updateUserTraining(training): void {
     this.user.training = training;
+    this.dateString = new Date(this.user.training.date).toDateString();
 
     const token = JSON.parse(localStorage.getItem('user')).token;
     const body = {token: token, training: training};
