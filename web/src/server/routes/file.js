ac2b70cb1f171c1418b601c54c35f634abec0330
Il Ung <ilungj@gmail.com>
2017-09-14 11:54:24 -1000

    Write file api.

---
+++ web/src/server/routes/file.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+router.get('/', (req, res) => {
+  /* ---- NEED AUTHENTICATION HERE ----*/
+
+  /* --------------------------------- */
+  let directory = __dirname; // Might have user define the directory
+  let list = [];
+  fs.readdir(directory, (err, files) => {
+    if(err) {
+      res.send('There is an error:', err);
+    } else {
+      for (let file of files) {
+        list.push({
+          name: file
+        });
+      }
+      res.send(list);
+    }
+  })
+});
+
+module.exports = router;
